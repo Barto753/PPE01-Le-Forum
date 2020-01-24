e044c38686a24cd69151ff5f7961c8b265958e4f
Barto753 <quentinguinta@orange.fr>
2020-01-24 11:06:58 +0100

    update

---
+++ PHP forum/header.php
@@ -0,0 +1,15 @@
+<?php
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<meta charset="utf-8" />
+		<title>Forum </title>
+        <link rel="icon" type="image/png" href="img/admin.png" />
+        <link rel="stylesheet" type="text/css" href="css/general.css" media="all"/>
+	</head>
+	<body>
