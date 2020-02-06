c629cbfb00ac0f4cb7bb00f58d6cf588b8fe3051
Barto753 <quentinguinta@orange.fr>
2020-02-06 09:52:52 +0100

    jeudi 06/02

---
+++ PHP forum/page Acueil/Acueuil.php
@@ -0,0 +1,22 @@
+<?php
+
+    include("header.php");
+?>
+	<body>
+		<div class="barre_navigation">
+			
+			<input type="submit" value="Connexion" class="button_connexion">
+			<input type="submit" value="Créer un compte" class="button_creation">
+
+		</div>
+		<div class="container_page">
+			<div class="header">
+				<img class="Acueil-logo" src="image/pickleRemover.jpg" alt="">
+				
+				<p class="accueil-titre"> Titre Forum </p>
+				
+			</div>
+		</div>
+<?php
+	include("footer.php");
+?>
---
+++ PHP forum/page Acueil/header.php
@@ -0,0 +1,7 @@
+<!DOCTYPE HTML>
+<html lang="fr">
+	<head>
+		<meta charset="utf-8" />
+		<title> Forum Page Acueuil </title>
+        <link rel="stylesheet" type="text/css" href="css/Acueuil.css" media="all"/>
+	</head>
