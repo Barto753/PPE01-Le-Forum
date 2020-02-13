7c4b479ccf743836ad662c8d083e5c29a9cbc206
Barto753 <quentinguinta@orange.fr>
2020-02-13 11:54:22 +0100

    Q-add

--- PHP forum/page Acueil/Acueuil.php
+++ PHP forum/page Acueil/Acueuil.php
@@ -3,20 +3,35 @@
     include("header.php");
 ?>
 	<body>
-		<div class="barre_navigation">
+		<div class="barre_entete">
 			
 			<input type="submit" value="Connexion" class="button_connexion">
 			<input type="submit" value="Créer un compte" class="button_creation">
 
 		</div>
-		<div class="container_page">
-			<div class="header">
-				<img class="Acueil-logo" src="image/pickleRemover.jpg" alt="">
+		<div class="header">
+			<img class="Acueil_logo" src="image/imageAcueuil.png" alt="">
 				
-				<p class="accueil-titre"> Titre Forum </p>
-				
-			</div>
+			<p class="accueil_titre"> Titre Forum </p>
 		</div>
+		<div class="barre_navigation">
+			<input type="submit" value="Menu" class="button_Menu">
+			<input type="submit" value="Page 1" class="button_firstPage">
+			<input type="submit" value="Page 2" class="button_ThirdPage">
+		</div>
+		<div class="forum_general">
+			<div class="favoris">
+				<p class="titre_barre"> Favoris </p>
+			</div>	
+			<div class="discussion">
+				<div class="entete_discussion">
+					<p class="discussion_titre"> Titre Forum </p>
+					<input type="submit" value="+" class="button_creaDiscution">
+				</div>
+			</div>
+
+		</div>
+		
 <?php
 	include("footer.php");
 ?>
