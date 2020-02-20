0162c597428e2d9fa0bf0fba98ea79bbab06589b
pdelahay <perrinedlh@gmail.com>
2020-02-20 08:30:36 +0100

    first commit p

---
+++ PHP Perrine/DatabaseLinker.php
@@ -0,0 +1,21 @@
+<?php
+
+    class DatabaseLinker
+    {
+	private static $urlBdd="mysql:host=localhost; dbname=bdd_forum; charset=utf8";
+	private static $username="root";
+	private static $password="root";
+	private static $connexionPdo;
+		
+	public static function getConnexion() #methode qui initialise l'attribue stockant la connexion
+	{
+            if(DatabaseLinker::$connexionPdo==null)
+            {
+		DatabaseLinker::$connexionPdo=new PDO(DatabaseLinker::$urlBdd,DatabaseLinker::$username,DatabaseLinker::$password);
+            }
+            return DatabaseLinker::$connexionPdo;
+	}
+    }
+
+?>
+
