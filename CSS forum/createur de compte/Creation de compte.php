ef58b7394b20dc4a7584b285a765f11bc072f66f
Unknown <unknown@example.com>
2020-03-12 11:51:27 +0100

    MAJ CSS crea compte

--- CSS forum/createur de compte/Creation de compte.php
+++ CSS forum/createur de compte/Creation de compte.php
@@ -6,22 +6,14 @@
 	<body>
 		<div class="container_page">
         <form method="post" action="ESSAI PPE\Panneau creation, connexioncss/Q-CreaCompte.css" name="login_form" class="div_connexion">
-			<div class="container_center">
+			<div class="container_center">	
 				<div>
-					<label for="idLogin">Prenom:</label>
-					<input type="text" id="idLogin" name="Prenom" class="input_login" required>
-				</div>
-				<div>
-					<label for="idLogin">nom:</label>
-					<input type="text" id="idLogin" name="nom" class="input_login" required>
+					<label for="idIdentifiant">Identifiant:</label>
+					<input type="text" id="idIdentifiant" name="Identifiant" class="input_login" required>
 				</div>
 				<div>
 					<label for="mail">E-mail:</label>
 					<input type="text" id="mail" name="mail" class="input_login" required>
-				</div>
-				<div>
-					<label for="idIdentifiant">Identifiant:</label>
-					<input type="text" id="idIdentifiant" name="Identifiant" class="input_login" required>
 				</div>
 				<div>
 					<label for="idPassword">Password:</label>
